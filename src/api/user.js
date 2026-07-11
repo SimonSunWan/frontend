c4c9013dc8dfc1048d9abbff82a5172f4b7854a1
Sunwan <18210921570@163.com>
2026-07-11 10:57:35 +0800

    接口名称

--- src/api/user.js
+++ src/api/user.js
@@ -1,41 +1,41 @@
 import request from '@/utils/request'
 
 export function loginApi(data) {
-  return request.post('/api/auth/login', data)
+  return request.post('/api/user/login', data)
 }
 
 export function getUserInfoApi() {
-  return request.get('/api/users/me')
+  return request.get('/api/user/me')
 }
 
 export function updateUserInfoApi(data) {
-  return request.put('/api/users/me', data)
+  return request.put('/api/user/me', data)
 }
 
 export function changePasswordApi(data) {
-  return request.put('/api/users/me/change-password', data)
+  return request.put('/api/user/me/change-password', data)
 }
 
 export function getUserListApi(params) {
-  return request.get('/api/users/', { params })
+  return request.get('/api/user/', { params })
 }
 
 export function createUserApi(data) {
-  return request.post('/api/users/', data)
+  return request.post('/api/user/', data)
 }
 
 export function updateUserApi(id, data) {
-  return request.put(`/api/users/${id}`, data)
+  return request.put(`/api/user/${id}`, data)
 }
 
 export function deleteUserApi(id) {
-  return request.delete(`/api/users/${id}`)
+  return request.delete(`/api/user/${id}`)
 }
 
 export function registerApi(data) {
-  return request.post('/api/users/register', data)
+  return request.post('/api/user/register', data)
 }
 
 export function forgetPasswordApi(data) {
-  return request.post('/api/users/forget-password', data)
+  return request.post('/api/user/forget-password', data)
 }
